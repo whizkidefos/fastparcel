9b22165357dc9ca194619c20dc26070397756a50
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2021-07-19 14:31:16 +0100

    added a base template

--- core/views.py
+++ core/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def customer_page(request):
+    return render(request, 'home.html')
+
+def courier_page(request):
+    return render(request, 'home.html')
--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('customer/', views.customer_page),
+    path('courier/', views.courier_page)
 ]
