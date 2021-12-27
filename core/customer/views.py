4ac79e59e040f919bb10840d6a956f7ecc608dca
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2021-12-27 11:09:50 +0000

    installed firebase

--- core/customer/views.py
+++ core/customer/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 from django.urls import reverse
 from core.customer import forms
 
@@ -14,19 +16,31 @@ def home(request):
 def profile_page(request):
     user_form = forms.BasicUserForm(instance=request.user)
     customer_form = forms.BasicCustomerForm(instance=request.user.customer)
+    password_form = PasswordChangeForm(request.user)
 
     if request.method == 'POST':
-        user_form = forms.BasicUserForm(request.POST, instance=request.user)
-        customer_form = forms.BasicCustomerForm(request.POST, request.FILES, instance=request.user.customer)
 
-        if user_form.is_valid() and customer_form.is_valid():
-            user_form.save()
-            customer_form.save()
+        if request.POST.get('action') == 'update_profile':
+            user_form = forms.BasicUserForm(request.POST, instance=request.user)
+            customer_form = forms.BasicCustomerForm(request.POST, request.FILES, instance=request.user.customer)
 
-            messages.success(request, 'Your profile has been updated.')
-            return redirect(reverse('customer:profile'))
+            if user_form.is_valid() and customer_form.is_valid():
+                user_form.save()
+                customer_form.save()
+
+                messages.success(request, 'Your profile has been updated.')
+                return redirect(reverse('customer:profile'))
+
+        elif request.POST.get('action') == 'update_password':
+            password_form = PasswordChangeForm(request.user, request.POST)
+            if password_form.is_valid():
+                user = password_form.save()
+
+                messages.success(request, 'Your password has been changed successfully.')
+                return redirect(reverse('customer:profile'))
 
     return render(request, 'customer/profile.html', {
         'user_form': user_form,
         'customer_form': customer_form,
+        'password_form': password_form,
     })
